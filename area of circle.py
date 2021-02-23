88d029e5f92e3925e2fb849952936f265df7c889
Parth271202 <73633177+Parth271202@users.noreply.github.com>
2021-02-23 15:10:32 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,5 @@
+import math #here math module is imported
+r=float(input('enter radius: '))
+area=math.pi*r**2 #pi is a constant in math module
+print('area of circle= ', area)
+print('area of circle={:0.2f}'.format(area))
